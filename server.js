531cfcd1b44deb685c930c0caedb1d8c73b0bcc9
Emran Hasan <63946687+raiyan24@users.noreply.github.com>
2022-08-26 11:30:57 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv');
 const path = require('path');
+const pageRouter = require('./router/pageRouter');
 
 
 //environment variable
@@ -17,25 +18,10 @@ app.use(express.urlencoded({extended : false}));
 //making public folder accessible
 app.use('/public',express.static('public'));
 
-//route
-app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname,'/public/index.html'));
-});
+//init router
+app.use(pageRouter);
 
-app.get('/menu', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname,'/public/menu.html'));
-});
 
-app.get('/book', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname,'/public/book.html'));
-});
-app.get('/about', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname,'/public/about.html'));
-});
-
-app.post('/book-order', (req, res) => {
-    res.status(200).json(req.body);
-});
 //listening server
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`.bgGreen.black);
